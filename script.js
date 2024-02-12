753d4438cb5b8daba03e3422daf84e6e80f27e3d
matysekf99 <126889853+matysekf99@users.noreply.github.com>
2024-02-12 20:58:28 +0100

    1ere version. il faut gérer le responsive

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ window.addEventListener("mousemove", function(e){
         top: `${posY}px`
     }, {duration: 300, fill: 'forwards'})
 });
+
+
+
+
+// pour augmenter la talle du curseur ( hover)
+
+// const buttons = document.querySelectorAll('.button_footer button');
+
+// // Pour chaque bouton, ajoutez un écouteur d'événements pour le survol
+// buttons.forEach(button => {
+//     button.addEventListener('mouseover', () => {
+//         // Sélectionnez l'élément .curseur et modifiez sa taille
+//         const curseur = document.querySelector('.curseur');
+//         curseur.style.width = '3em';
+//         curseur.style.height = '3em';
+//     });
+
+//     button.addEventListener('mouseout', () => {
+//         // Réinitialisez la taille de l'élément .curseur lorsque la souris quitte le bouton
+//         const curseur = document.querySelector('.curseur');
+//         curseur.style.width = '2em';
+//         curseur.style.height = '2em';
+//     });
+// });
